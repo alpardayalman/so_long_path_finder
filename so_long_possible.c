450375db9ecf61ea62ea3515496d9a57f6925b61
Martini <82611850+alpardayalman@users.noreply.github.com>
2023-05-21 22:16:58 +0300

    Update so_long_possible.c

--- so_long_possible.c
+++ so_long_possible.c
@@ -1,32 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
 
 typedef struct s_game {
-	char			**map;
-	size_t			width;
-	size_t			height;
-	size_t			coin;
-	size_t			player_x; // player
-	size_t			player_y;
-	size_t			exit_is_possible;
-	size_t			exit_y;
+	char			**map; // Your own map.
+	size_t			width; // The width of your map.
+	size_t			height; // The height of your map.
+	size_t			coin; // Collectible
+	size_t			player_x; // player pos X
+	size_t			player_y; // player pos Y
+	size_t			exit_is_possible; // You need to add this to your own struct (Kendi struct'una eklemeyi unutma)
 }	t_game;
 
-void	free_map(char **map, size_t height)
+void	free_map(char **map, size_t height) // Free the map, you can use your own.
 {
-	size_t	i;
+	int	i;
 
-	i = 0;
-	while (i < height)
-	{
+	i = -1;
+	while (++i < height)
 		free(map[i]);
-		i++;
-	}
 	free(map);
 }
-
+// Recursive manner, checks if it is possible to exit whilst collecting the collectible. (DFS search)
 int	check_path(t_game *temp, size_t y, size_t x)
 {
 	if (temp->map[y][x] == '1')
@@ -71,44 +66,38 @@ void	flood_fill(t_game *game)
 		i++;
 	}
 	check_path(&temp, temp.player_y, temp.player_x);
-	if (!(temp.exit_is_possible == 1 && temp.coin == 0))
-    {
-        printf("hatali map");
-    }
-    else
-        printf("gecerli map");
+	if (!(temp.exit_is_possible == 1 && temp.coin == 0)) // Check if the player could exit and all the coins are collected.
+		printf("Faulty Map");
+	else
+		printf("Valid Map");
 	free_map(temp.map, temp.height);
 }
 
 
 int main() {
 
-    t_game *game = (t_game *)malloc(sizeof(t_game));
-
-    game->coin = 2; // Number of collectible
-    game->exit_is_possible = 0; // is exit possible
-    game->height = 10; // height of map
-    game->player_x = 10; // player x
-    game->player_y = 6; // player y
-
-
-    game->map = (char **)malloc(sizeof(char *) * 10);
-    game->map[0] = strdup("1111111111111111111111111");
-    game->map[1] = strdup("1000000000000010000000001");
-    game->map[2] = strdup("100001E000000010000000001");
-    game->map[3] = strdup("1000010000000010000000001");
-    game->map[4] = strdup("100001000C000010000000001");
-    game->map[5] = strdup("1000010000000010000000001");
-    game->map[6] = strdup("1000100000P00010000000001");
-    game->map[7] = strdup("1000101001000011110C00001");
-    game->map[8] = strdup("1000001000000010000000001");
-    game->map[9] = strdup("1111111111111111111111111");
-    
-    game->width = strlen(game->map[0]);
-    flood_fill(game);
+	t_game *game = (t_game *)malloc(sizeof(t_game));
+	// You should already get these values before. These are custom values depending on my custom map.
+	game->coin = 2; // Number of collectible
+	game->exit_is_possible = 0; // is exit possible
+	game->height = 10; // height of map
+	game->player_x = 10; // player x
+	game->player_y = 6; // player y
 
 
-    // Free memory allocated for the map
+	game->map = (char **)malloc(sizeof(char *) * 10); // Random map of choosing.
+	game->map[0] = strdup("1111111111111111111111111");
+	game->map[1] = strdup("1000000000000010000000001");
+	game->map[2] = strdup("100001E000000010000000001");
+	game->map[3] = strdup("1000010000000010000000001");
+	game->map[4] = strdup("100001000C000010000000001");
+	game->map[5] = strdup("1000010000000010000000001");
+	game->map[6] = strdup("1000100000P00010000000001");
+	game->map[7] = strdup("1000101001000011110C00001");
+	game->map[8] = strdup("1000001000000010000000001");
+	game->map[9] = strdup("1111111111111111111111111");
 
-    return 0;
+	game->width = strlen(game->map[0]);
+	flood_fill(game);
+	return 0;
 }
